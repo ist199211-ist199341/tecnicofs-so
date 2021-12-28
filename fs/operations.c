6c0ccd347da1de04f94569d3f541a43142b10e00
Pesteves2002 <tomasesteves2002@gmail.com>
2021-12-28 14:43:55 +0000

    Add part 2

--- fs/operations.c
+++ fs/operations.c
@@ -223,3 +223,24 @@ ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
 
     return (ssize_t)read;
 }
+
+int tfs_copy_to_external_fs(char const *source_path, char const *dest_path) {
+
+    int inumber_file = tfs_open(source_path, 0);
+    /* if file doesn't exist */
+    if (inumber_file == -1) {
+        return -1;
+    }
+    /* flag "w" - crates empty file, if it exists it clears the content :) */
+    FILE *dest_file = fopen(dest_path, "w");
+    if (dest_file == NULL) {
+        return -1;
+    }
+    char buffer[BLOCK_SIZE];
+
+    ssize_t read;
+    while ((read = tfs_read(inumber_file, buffer, BLOCK_SIZE)) > 0)
+        fwrite(buffer, sizeof(char), (size_t)read, dest_file);
+    fclose(dest_file);
+    return 0;
+}
