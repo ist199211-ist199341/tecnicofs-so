bb26a4476d46d357925dfb095b442e7dfd2c8d33
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2022-01-12 14:19:13 +0000

    Add comments to util.c

--- fs/utils.c
+++ fs/utils.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * Locks the wrlock to write-read.
+ * Exits with failure if unsuccessful.
+ */
 void rwl_wrlock(pthread_rwlock_t *rwl) {
     if (pthread_rwlock_wrlock(rwl) != 0) {
         perror("Failed to lock RWlock");
@@ -9,6 +13,10 @@ void rwl_wrlock(pthread_rwlock_t *rwl) {
     }
 }
 
+/*
+ * Locks the wrlock to read-only.
+ * Exits with failure if unsuccessful.
+ */
 void rwl_rdlock(pthread_rwlock_t *rwl) {
     if (pthread_rwlock_rdlock(rwl) != 0) {
         perror("Failed to lock RWlock");
@@ -16,6 +24,10 @@ void rwl_rdlock(pthread_rwlock_t *rwl) {
     }
 }
 
+/*
+ * Unlocks the wrlock.
+ * Exits with failure if unsuccessful.
+ */
 void rwl_unlock(pthread_rwlock_t *rwl) {
     if (pthread_rwlock_unlock(rwl) != 0) {
         perror("Failed to unlock RWlock");
@@ -23,13 +35,20 @@ void rwl_unlock(pthread_rwlock_t *rwl) {
     }
 }
 
+/*
+ * Locks the mutex.
+ * Exits with failure if unsuccessful.
+ */
 void mutex_lock(pthread_mutex_t *mutex) {
     if (pthread_mutex_lock(mutex) != 0) {
         perror("Failed to lock Mutex");
         exit(EXIT_FAILURE);
     }
 }
-
+/*
+ * Unlocks the mutex.
+ * Exits with failure if unsuccessful.
+ */
 void mutex_unlock(pthread_mutex_t *mutex) {
     if (pthread_mutex_unlock(mutex) != 0) {
         perror("Failed to unlock Mutex");
