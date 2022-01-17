47b33d971fdb876126bf73524e5dcdb5e2ec318e
Diogo Correia <me@diogotc.com>
2022-01-17 14:11:29 +0000

    Suppress non-used variables on yet-to-implement code

--- client/tecnicofs_client_api.c
+++ client/tecnicofs_client_api.c
@@ -1,6 +1,8 @@
 #include "tecnicofs_client_api.h"
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
+    (void)client_pipe_path;
+    (void)server_pipe_path;
     /* TODO: Implement this */
     return -1;
 }
@@ -11,21 +13,30 @@ int tfs_unmount() {
 }
 
 int tfs_open(char const *name, int flags) {
+    (void)name;
+    (void)flags;
     /* TODO: Implement this */
     return -1;
 }
 
 int tfs_close(int fhandle) {
+    (void)fhandle;
     /* TODO: Implement this */
     return -1;
 }
 
 ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
+    (void)fhandle;
+    (void)buffer;
+    (void)len;
     /* TODO: Implement this */
     return -1;
 }
 
 ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
+    (void)fhandle;
+    (void)buffer;
+    (void)len;
     /* TODO: Implement this */
     return -1;
 }
