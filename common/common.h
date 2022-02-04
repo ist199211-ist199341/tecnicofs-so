4aed04a3777715b0caeefb58d2342b07e6a67a41
Pesteves2002 <tomasesteves2002@gmail.com>
2022-02-04 21:47:58 +0000

    Change order of function on common.h

--- common/common.h
+++ common/common.h
@@ -36,15 +36,15 @@ ssize_t try_read(int fd, void *buf, size_t count);
  */
 ssize_t try_write(int fd, const void *buf, size_t count);
 
-/* check if all the content was written to the pipe. */
-#define write_pipe(pipe, buffer, size)                                         \
-    if (try_write(pipe, buffer, size) != size) {                               \
-        return -1;                                                             \
-    }
-
 /* check if all the content was read from the pipe. */
 #define read_pipe(pipe, buffer, size)                                          \
     if (try_read(pipe, buffer, size) != size) {                                \
+        return -1;                                                             \
+    }
+
+/* check if all the content was written to the pipe. */
+#define write_pipe(pipe, buffer, size)                                         \
+    if (try_write(pipe, buffer, size) != size) {                               \
         return -1;                                                             \
     }
 
